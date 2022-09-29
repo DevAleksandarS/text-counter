282743c446a74773b3cd30afafe3e700330f33c8
DevAleksandarS <spasojevic.dev@gmail.com>
2022-09-29 15:42:31 +0200

    Styling commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import "./css/style.css";
+
 function App() {
   return (
     <div className="main-container">
-      <div></div>
+      <h1>Text Counter</h1>
+      <div>
+        <textarea></textarea>
+        <div className="display-area">
+          <p>Characters, Words, Sentences, Lines, Symbols</p>
+        </div>
+      </div>
     </div>
   );
 }
